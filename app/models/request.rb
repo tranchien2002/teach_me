86b69f1ccb57df8f5d766801515e469641c7ef49
tranchien2002 <chie.tm2002@gmail.com>
2018-02-28 21:11:55 +0700

    create request & modal login

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,9 @@
 class Request < ApplicationRecord
+  enum topic: {math: 0, it: 1, literary: 2, history: 3, chemistry:4, physics: 5, other: 6}
+  enum status: {looking: 0, done: 1}
   has_many :applies, dependent: :destroy
   belongs_to :user
+  validates :bill, presence: true, numericality: { only_float: true },format: {with: /[0-9]*\.[0-9]*/}
+  validates :header, presence: true, length: {minimum: 10}
+  validates :content, presence: true, length: {minimum: 20}
 end
