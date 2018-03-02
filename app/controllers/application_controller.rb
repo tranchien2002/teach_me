abe94878326c5ecb377751a261a6d05ff9860303
Nguyen Dinh Hieu <dinhhieu16598@gmail.com>
2018-03-02 19:09:02 +0700

    show profile user and fixed i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_locale
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit :sign_up, keys: [:name, :sex, :address, :avatar, :description]
+  end
 
   private
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options
+    {locale: I18n.locale}
   end
 end
