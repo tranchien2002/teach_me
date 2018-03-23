5216171cdcf20862e5e8a4da17153b2915678103
Nguyen Dinh Hieu <dinhhieu16598@gmail.com>
2018-03-23 19:18:57 +0700

    Customize redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,14 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || super
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    URI.parse(request.referer).path if request.referer
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit :sign_up, keys: [:name, :sex, :address, :avatar, :description]
   end
